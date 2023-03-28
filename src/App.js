b81dbb62bd301d0af713070ebf491c466c2efce0
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-28 15:59:15 +0000

    Add logo image and NavBar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 // import { Button } from 'bootstrap';
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to SpoodleSpace fellow Spoodles and Cockapoopers!</h1>
+      {/* <h1>Welcome to SpoodleSpace fellow Spoodles and Cockapoopers!</h1>
       
       <div id='container-one'>
         <div id='ss-logo'></div>
-      </div>
+      </div> */}
 
-      <Button variant="primary">Primary</Button>
+      <NavBar />
+
+      {/* <Button variant="primary">Click Me</Button> */}
 
     </div>
   );
