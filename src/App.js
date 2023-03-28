a689f47bb6894335cfd9a138c664c506096bae66
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-28 19:04:52 +0000

    Add css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import { Button } from 'bootstrap';
 // import Button from "react-bootstrap/Button";
-import './App.css';
+import styles from './App.module.css';
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="styles.App">
       {/* <h1>Welcome to SpoodleSpace fellow Spoodles and Cockapoopers!</h1>
       
       <div id='container-one'>
