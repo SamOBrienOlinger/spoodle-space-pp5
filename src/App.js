b8d2b437c009542f958a363032ae070f92632709
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-28 15:26:58 +0000

    Add Button to Apps.js file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// import { Button } from 'bootstrap';
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
@@ -6,12 +8,10 @@ function App() {
       <h1>Welcome to SpoodleSpace fellow Spoodles and Cockapoopers!</h1>
       
       <div id='container-one'>
-        
-        <div id='ss-logo'>
-
-        </div>
-      
+        <div id='ss-logo'></div>
       </div>
+
+      <Button variant="primary">Primary</Button>
 
     </div>
   );
