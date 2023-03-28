b9376a76e70442247c562423395ddd2d59cb6a1f
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-28 10:00:09 +0000

    Update welcome message in Apps.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome fellow Spoodles and Cockapoopers!</h1>
+      <h1>Welcome to SpoodleSpace fellow Spoodles and Cockapoopers!</h1>
       <div id='ss-logo'>
 
       </div>
