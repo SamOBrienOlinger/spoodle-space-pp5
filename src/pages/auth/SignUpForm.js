6f9aeea54a5f51fa89a0d85a0fb17ac096f44c3b
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-30 12:44:22 +0000

    Fix bugs with relative file paths for ./pages/auth

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -5,19 +5,19 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import {
-  Form,
-  Button,
-  Image,
-  Col,
-  Row,
-  Container,
-  Alert,
-} from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+
 import axios from "axios";
+// import { useRedirect } from "../../hooks/useRedirect";
 
 const SignUpForm = () => {
-
+  // useRedirect("loggedIn");
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
@@ -54,82 +54,73 @@ const SignUpForm = () => {
 
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control 
-              className={styles.Input}
-              type="text" 
-              placeholder="username" 
-              name="username"
-              value={username}
-              onChange={handleChange}
-              />            
-            
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
             </Form.Group>
-
             {errors.username?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
 
-
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
-              className={styles.Input} 
-              type="password" 
-              placeholder="Password"
-              name="password1" 
-              value={password1}
-              onChange={handleChange}
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
               />
-
-            {errors.password1?.map((message, idx) => (
-                          <Alert key={idx} variant="warning">
-                            {message}
-                          </Alert>
-                        ))}
-
             </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm password</Form.Label>
               <Form.Control
-              className={styles.Input} 
-              type="password" 
-              placeholder="Confirm Password"
-              name="password2" 
-              value={password2}
-              onChange={handleChange}
-            />
-          
+                className={styles.Input}
+                type="password"
+                placeholder="Confirm password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
             </Form.Group>
-
             {errors.password2?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
 
-
-            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               type="submit"
             >
               Sign up
             </Button>
-
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
             ))}
-
           </Form>
-
         </Container>
+
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
-            Do you already have an account with us? <span>Sign in</span>
+            Already have an account? <span>Sign in</span>
           </Link>
         </Container>
       </Col>
