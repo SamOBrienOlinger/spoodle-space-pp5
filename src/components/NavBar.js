c2429d7f5875d9076119bf4dc0512e036041bcb3
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-30 14:56:12 +0000

    Fix bug with data undefined in SignInForm.js file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo-spoodlespace.webp";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -13,33 +36,20 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
-              // exact
+              exact
               className={styles.NavLink}
               activeClassName={styles.Active}
               to="/"
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink
-              exact
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i className="fas fa-sign-in-alt"></i>Login
-            </NavLink>
-            <NavLink
-              exact
-              to="/signup"
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-            >
-              <i className="fas fa-user-plus"></i>Join
-            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -48,6 +58,87 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+
+
+// import React, { useContext } from "react";
+// import { Navbar, Container, Nav } from "react-bootstrap";
+// import logo from "../assets/logo-spoodlespace.webp";
+// import styles from "../styles/NavBar.module.css";
+// import { NavLink } from "react-router-dom";
+// import { CurrentUserContext } from "../App";
+
+// const NavBar = () => {
+//   const currentUser = useContext(CurrentUserContext);
+
+//   const loggedInIcons = <>{currentUser?.username}</>;
+//   const loggedOutIcons = (
+//     <>
+//       <NavLink
+//         className={styles.NavLink}
+//         activeClassName={styles.Active}
+//         to="/signin"
+//       >
+//         <i className="fas fa-sign-in-alt"></i>Sign in
+//       </NavLink>
+//       <NavLink
+//         to="/signup"
+//         className={styles.NavLink}
+//         activeClassName={styles.Active}
+//       >
+//         <i className="fas fa-user-plus"></i>Sign up
+//       </NavLink>
+//     </>
+//   );
+
+//   return (
+//     <Navbar className={styles.NavBar} expand="md" fixed="top">
+//       <Container>
+//         <NavLink to="/">
+//           <Navbar.Brand>
+//             <img src={logo} alt="logo" height="45" />
+//           </Navbar.Brand>
+//         </NavLink>
+
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="ml-auto text-left">
+//             <NavLink
+//               // exact
+//               className={styles.NavLink}
+//               activeClassName={styles.Active}
+//               to="/"
+//             >
+//               <i className="fas fa-home"></i>Home
+//             </NavLink>
+
+//             <NavLink
+//               exact
+//               className={styles.NavLink}
+//               activeClassName={styles.Active}
+//               to="/signin"
+//             >
+//               <i className="fas fa-sign-in-alt"></i>Login
+//             </NavLink>
+
+//             <NavLink
+//               exact
+//               to="/signup"
+//               className={styles.NavLink}
+//               activeClassName={styles.Active}
+//             >
+
+//               <i className="fas fa-user-plus"></i>Join
+//             </NavLink>
+
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// };
+
+// export default NavBar;
 
 
 // import React from "react";
