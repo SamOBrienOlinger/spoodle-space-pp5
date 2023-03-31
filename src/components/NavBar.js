55072aaecc8f4f9bfc8b5618b6a290f214f71bce
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-31 10:38:56 +0000

    Update logo and add

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/logo-spoodlespace.webp";
+import logo from "../assets/logo2-spoodlespace.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import {
