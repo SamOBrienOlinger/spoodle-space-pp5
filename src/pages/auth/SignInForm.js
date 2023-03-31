85be65f3c1600c075cb7a771ce9e08c9c950f6fa
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-31 15:21:36 +0000

    Add console.log/Line 36/SignInForm.js file

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -33,6 +33,7 @@ function SignInForm() {
     
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+        console.log('data', data);
       setCurrentUser(data.user);
       history.push("/");
     } catch (err) {
