0e6635a3905ca77c5c5919eaec326fd36e5d6439
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-04 14:23:19 +0000

    Add Posts.js and relevant file changes

--- src/App.js
+++ src/App.js
@@ -9,12 +9,35 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 
 function App() {
+
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Welcome to SpoodleSpace!</h1>} />
+          {/* <Route exact path="/" render={() => <h1>Welcome to SpoodleSpace!</h1>} /> */}
+
+<Route
+          exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
