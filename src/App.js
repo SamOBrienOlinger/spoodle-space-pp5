7f29a56a1d3d04f81757b0584621b939ac4c53b9
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-04 12:09:15 +0000

    Update homepage content

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/" render={() => <h1>Welcome to SpoodleSpace!</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
