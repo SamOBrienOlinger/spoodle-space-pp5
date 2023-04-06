9914f78fb15d7e77a771695dd7663309f18c5421
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-06 12:05:17 +0000

    Add posts to the ProfilePage.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
-// import ProfilePage from "./pages/profiles/ProfilePage";
+import ProfilePage from "./pages/profiles/ProfilePage";
 // import UsernameForm from "./pages/profiles/UsernameForm";
 // import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 // import ProfileEditForm from "./pages/profiles/ProfileEditForm";
@@ -58,29 +58,8 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <p>Page not found!</p>} />
-
-
-          {/* <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-
-          <Route
-            exact
-            path="/profiles/:id/edit/username"
-            render={() => <UsernameForm />}
-          />
-          <Route
-            exact
-            path="/profiles/:id/edit/password"
-            render={() => <UserPasswordForm />}
-          />
-          <Route
-            exact
-            path="/profiles/:id/edit"
-            render={() => <ProfileEditForm />}
-          />
-
-        <Route render={() => <NotFound />}
-        /> */}
         </Switch>
       </Container>
     </div>
