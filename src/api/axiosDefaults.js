766dd7cd56d7847dfe9584cfa0f5055c6dea6760
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-09 13:36:27 +0000

    Update baseURL to spoodle-space-pp5.herokuapp

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
+// axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
+axios.defaults.baseURL = "https://spoodle-space-pp5.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
