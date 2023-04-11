95d6c78630c80a1101a62dd8d3822c6c760bdab1
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-11 15:22:29 +0000

    Fix issue with .length

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -60,7 +60,8 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
-          {comments.results.length ? (
+          {/* {comments.results.length ? ( */}
+          {comments?.results?.length ? ( 
             <InfiniteScroll
               children={comments.results.map((comment) => (
                 <Comment
