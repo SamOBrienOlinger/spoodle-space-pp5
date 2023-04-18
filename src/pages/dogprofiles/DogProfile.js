b5585c87c3b1f4d726e19c0fcfb98c291e7d67c8
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-18 13:56:11 +0000

    Test pages render

--- src/pages/dogprofiles/DogProfile.js
+++ src/pages/dogprofiles/DogProfile.js
@@ -7,49 +7,9 @@
 // import { Button } from "react-bootstrap";
 // import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
-// const DogProfile = (props) => {
-//   const { profile, mobile, imageSize = 55 } = props;
-//   const { id, following_id, image, owner } = profile;
+function DogProfilePage() {
+    console.log('Hello, doggy profiles page!');
+    }
+    DogProfilePage();
 
-//   const currentUser = useCurrentUser();
-//   const is_owner = currentUser?.username === owner;
-
-//   const { handleFollow } = useSetProfileData();
-
-//   return (
-//     <div
-//       className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
-//     >
-//       <div>
-//         <Link className="align-self-center" to={`/profiles/${id}`}>
-//           <Avatar src={image} height={imageSize} />
-//         </Link>
-//       </div>
-//       <div className={`mx-2 ${styles.WordBreak}`}>
-//         <strong>{owner}</strong>
-//       </div>
-//       <div className={`text-right ${!mobile && "ml-auto"}`}>
-//         {!mobile &&
-//           currentUser &&
-//           !is_owner &&
-//           (following_id ? (
-//             <Button
-//               className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
-//               onClick={() => {}}
-//             >
-//               unfollow
-//             </Button>
-//           ) : (
-//             <Button
-//               className={`${btnStyles.Button} ${btnStyles.Black}`}
-//               onClick={() => handleFollow(profile)}
-//             >
-//               follow
-//             </Button>
-//           ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DogProfile
+export default DogProfiles;
