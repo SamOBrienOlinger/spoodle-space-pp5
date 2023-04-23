313a0fc5d5864f4e909e9b97a6126be7ca5cd334
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-23 15:36:47 +0000

    Add return statement

--- src/pages/dogprofiles/DogProfile.js
+++ src/pages/dogprofiles/DogProfile.js
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "../../styles/Post.module.css";
+// import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 import Card from "react-bootstrap/Card";
 import Media from "react-bootstrap/Media";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
+// import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+// import Tooltip from "react-bootstrap/Tooltip";
 
 import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
@@ -19,6 +19,18 @@ const DogProfile = (props) => {
     profile_id,
     profile_image,
     updated_at,
+    // 'id',
+    // 'owner',
+    // 'is_owner',
+    // 'created_at',
+    // 'updated_at',
+    // 'profile_id',
+
+    dog_name,
+    dog_age,
+    dog_color,
+    dog_bio,
+    dog_profile_image,
 
     dogProfilePage,
     setDogProfiles
@@ -27,6 +39,7 @@ const DogProfile = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
   const history = useHistory();
+  const [showEditForm, setShowEditForm] = useState(false);
 
   const handleEdit = () => {
     history.push(`/dogprofiles/${id}/edit`);
@@ -41,46 +54,35 @@ const DogProfile = (props) => {
     }
   };
 
-  const handleLike = async () => {
-    try {
-      const { data } = await axiosRes.post("/likes/", { post: id });
-      setDogProfiles((prevDogProfiles) => ({
-        ...prevDogProfiles,
-        results: prevDogProfiles.results.map((post) => {
-          return post.id === id
-            ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
-            : post;
-        }),
-      }));
-    } catch (err) {
-      // console.log(err);
-    }
-  };
-
-  const handleUnlike = async () => {
-    try {
-      await axiosRes.delete(`/likes/${like_id}/`);
-      setDogProfiles((prevDogProfiles) => ({
-        ...prevDogProfiles,
-        results: prevDogProfiles.results.map((post) => {
-          return post.id === id
-            ? { ...post, likes_count: post.likes_count - 1, like_id: null }
-            : post;
-        }),
-      }));
-    } catch (err) {
-      // console.log(err);
-    }
-  };
+  // const handleLike = async () => {
+  //   try {
+  //     const { data } = await axiosRes.post("/likes/", { post: id });
+  //     setDogProfiles((prevDogProfiles) => ({
+  //       ...prevDogProfiles,
+  //       results: prevDogProfiles.results.map((post) => {
+  //         return post.id === id
+  //           ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+  //           : post;
+  //       }),
+  //     }));
+  //   } catch (err) {
+  //     // console.log(err);
+  //   }
+  // };
 
   return (
-    <Card className={styles.Post}>
-      <Card.Body>
+    <>
+      <hr />
+      <div>
+        
         <Media className="align-items-center justify-content-between">
           <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
+            <Avatar src={profile_image} />
             {owner}
           </Link>
+          <Link to={`/dogprofiles/${id}`}>
+          <Card.Img src={dog_profile_image} />
+        </Link>
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
             {is_owner && dogProfilePage && (
@@ -90,47 +92,31 @@ const DogProfile = (props) => {
               />
             )}
           </div>
-        </Media>
-      </Card.Body>
-      <Link to={`/dogprofiles/${id}`}>
-        <Card.Img src={image} alt={title} />
-      </Link>
-      <Card.Body>
-        {title && <Card.Title className="text-center">{title}</Card.Title>}
-        {content && <Card.Text>{content}</Card.Text>}
-        <div className={styles.PostBar}>
-          {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't like your own post!</Tooltip>}
-            >
-              <i className="far fa-heart" />
-            </OverlayTrigger>
-          ) : like_id ? (
-            <span onClick={handleUnlike}>
-              <i className={`fas fa-heart ${styles.Heart}`} />
-            </span>
-          ) : currentUser ? (
-            <span onClick={handleLike}>
-              <i className={`far fa-heart ${styles.HeartOutline}`} />
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Log in to like posts!</Tooltip>}
-            >
-              <i className="far fa-heart" />
-            </OverlayTrigger>
-          )}
-          {likes_count}
-          <Link to={`/dogprofiles/${id}`}>
-            <i className="far fa-comments" />
-          </Link>
-          {comments_count}
-        </div>
-      </Card.Body>
-    </Card>
-  );
+          <Media.Body className="align-self-center ml-2">
+            <span className={styles.Owner}>{owner}</span>
+            <span className={styles.Date}>{updated_at}</span>
+            {showEditForm ? (
+              <DogProfileEditForm
+                id={id}
+                profile_id={profile_id}
+
+                dog_name = {dog_name}
+                dog_age = {dog_age}
+                dog_color = {dog_color}
+                dog_bio = {dog_bio}
+                dog_profile_image = {dog_profile_image}
+                setDogProfiles={setDogProfiles}
+                setShowEditForm={setShowEditForm}
+              />
+            ) : (
+              <p></p>
+            )}
+          </Media.Body>
+
+      </Media>
+    </div>
+  </>
+ );
 };
 
 export default DogProfile;
