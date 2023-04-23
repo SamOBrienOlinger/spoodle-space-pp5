3aa8890a14fe082b4ac747591cdad80be2264d80
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-23 17:06:23 +0000

    Remove all code to try and render DogProfilePage

--- src/pages/dogprofiles/DogProfile.js
+++ src/pages/dogprofiles/DogProfile.js
@@ -1,122 +1,55 @@
 import React from "react";
-// import styles from "../../styles/Post.module.css";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
-import Card from "react-bootstrap/Card";
-import Media from "react-bootstrap/Media";
-// import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-// import Tooltip from "react-bootstrap/Tooltip";
-
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
-import { axiosRes } from "../../api/axiosDefaults";
-import { MoreDropdown } from "../../components/MoreDropdown";
+import { Button } from "react-bootstrap";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 const DogProfile = (props) => {
-  const {
-    id,
-    owner,
-    profile_id,
-    profile_image,
-    updated_at,
-    // 'id',
-    // 'owner',
-    // 'is_owner',
-    // 'created_at',
-    // 'updated_at',
-    // 'profile_id',
-
-    dog_name,
-    dog_age,
-    dog_color,
-    dog_bio,
-    dog_profile_image,
-
-    dogProfilePage,
-    setDogProfiles
-  } = props;
+  const { dogprofile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = dogprofile;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
-  const history = useHistory();
-  const [showEditForm, setShowEditForm] = useState(false);
 
-  const handleEdit = () => {
-    history.push(`/dogprofiles/${id}/edit`);
-  };
-
-  const handleDelete = async () => {
-    try {
-      await axiosRes.delete(`/dogprofiles/${id}/`);
-      history.goBack();
-    } catch (err) {
-      // console.log(err);
-    }
-  };
-
-  // const handleLike = async () => {
-  //   try {
-  //     const { data } = await axiosRes.post("/likes/", { post: id });
-  //     setDogProfiles((prevDogProfiles) => ({
-  //       ...prevDogProfiles,
-  //       results: prevDogProfiles.results.map((post) => {
-  //         return post.id === id
-  //           ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
-  //           : post;
-  //       }),
-  //     }));
-  //   } catch (err) {
-  //     // console.log(err);
-  //   }
-  // };
+  const { handleFollow } = useSetProfileData();
 
   return (
-    <>
-      <hr />
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
       <div>
-        
-        <Media className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} />
-            {owner}
-          </Link>
-          <Link to={`/dogprofiles/${id}`}>
-          <Card.Img src={dog_profile_image} />
+        <Link className="align-self-center" to={`/dogprofiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
         </Link>
-          <div className="d-flex align-items-center">
-            <span>{updated_at}</span>
-            {is_owner && dogProfilePage && (
-              <MoreDropdown
-                handleEdit={handleEdit}
-                handleDelete={handleDelete}
-              />
-            )}
-          </div>
-          <Media.Body className="align-self-center ml-2">
-            <span className={styles.Owner}>{owner}</span>
-            <span className={styles.Date}>{updated_at}</span>
-            {showEditForm ? (
-              <DogProfileEditForm
-                id={id}
-                profile_id={profile_id}
-
-                dog_name = {dog_name}
-                dog_age = {dog_age}
-                dog_color = {dog_color}
-                dog_bio = {dog_bio}
-                dog_profile_image = {dog_profile_image}
-                setDogProfiles={setDogProfiles}
-                setShowEditForm={setShowEditForm}
-              />
-            ) : (
-              <p></p>
-            )}
-          </Media.Body>
-
-      </Media>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => handleFollow(dogprofile)}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
     </div>
-  </>
- );
+  );
 };
 
 export default DogProfile;
