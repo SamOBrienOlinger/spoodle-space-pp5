89ab6a19e973dec63196b24acceac5239bee6fd9
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-05-14 14:32:48 +0000

    Update baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,7 +3,10 @@ import axios from "axios";
 // axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu94.gitpod.io";
 // axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu95.gitpod.io";
 // axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu96.gitpod.io";
-axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu96b.gitpod.io";
+// axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu96b.gitpod.io";
+
+axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu97.gitpod.io"
+
 // axios.defaults.baseURL = "https://spoodlespace.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
