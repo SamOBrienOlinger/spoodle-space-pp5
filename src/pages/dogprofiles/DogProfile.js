ac20570b78f2ef8811849d6182cee6fb86f77873
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-05-15 14:36:46 +0000

    Bug re <Link to={`/dogprofile/${id}`}>           <Card.Img src={dog_profile_image} alt={dog_name} />         </Link>

--- src/pages/dogprofiles/DogProfile.js
+++ src/pages/dogprofiles/DogProfile.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "../../styles/DogProfile.module.css";
-// import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 import { Link, useHistory } from "react-router-dom";
 // import Avatar from "../../components/Avatar";
