e68055e75037fbb6fd1ac1c2959de4286309a749
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-05-23 12:38:31 +0000

    comment out imports until ready to use

--- src/pages/dogdanger/DogDangersPage.js
+++ src/pages/dogdanger/DogDangersPage.js
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+// import React, { useEffect, useState } from "react";
+// import Form from "react-bootstrap/Form";
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Container from "react-bootstrap/Container";
 
-import DogDanger from "./DogDanger";
-import Asset from "../../components/Asset";
+// import DogDanger from "./DogDanger";
+// import Asset from "../../components/Asset";
 
-import appStyles from "../../App.module.css";
-import styles from "../../styles/DogProfilesPage.module.css";
-import { useLocation } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
+// import appStyles from "../../App.module.css";
+// import styles from "../../styles/DogProfilesPage.module.css";
+// import { useLocation } from "react-router";
+// import { axiosReq } from "../../api/axiosDefaults";
 
-import NoResults from "../../assets/no-results.png";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchMoreData } from "../../utils/utils";
-import PopularProfiles from "../profiles/PopularProfiles";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+// import NoResults from "../../assets/no-results.png";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import { fetchMoreData } from "../../utils/utils";
+// import PopularProfiles from "../profiles/PopularProfiles";
+// import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function DogDangersPage() {
     return <p className="text-center">hello doggy danger world</p>;
--- src/pages/doghealth/DogsHealthPage.js
+++ src/pages/doghealth/DogsHealthPage.js
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+// import React, { useEffect, useState } from "react";
 
-import DogHealth from "./DogHealth";
-import Asset from "../../components/Asset";
+// import Form from "react-bootstrap/Form";
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Container from "react-bootstrap/Container";
 
-import appStyles from "../../App.module.css";
-import styles from "../../styles/DogProfilesPage.module.css";
-import { useLocation } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
+// import DogHealth from "./DogHealth";
+// import Asset from "../../components/Asset";
 
-import NoResults from "../../assets/no-results.png";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchMoreData } from "../../utils/utils";
-import PopularProfiles from "../profiles/PopularProfiles";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+// import appStyles from "../../App.module.css";
+// import styles from "../../styles/DogProfilesPage.module.css";
+// import { useLocation } from "react-router";
+// import { axiosReq } from "../../api/axiosDefaults";
+
+// import NoResults from "../../assets/no-results.png";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import { fetchMoreData } from "../../utils/utils";
+// import PopularProfiles from "../profiles/PopularProfiles";
+// import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function DogsHealthPage() {
     return <p className="text-center">hello doggy health world</p>;
