9a8d958f2c44c5907396007a3f1b29845de27e23
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-07-19 12:51:54 +0000

    Update gitpod url for development mode

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-//   "https://8000-samobrienol-drfspoodles-t68akmfonum.ws-eu100.gitpod.io/";
-axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
+axios.defaults.baseURL = "https://samobrienol-drfspoodles-ufklcxsviem.ws-eu101.gitpod.io";
+// axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
