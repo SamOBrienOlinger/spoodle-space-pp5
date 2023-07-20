85c9afa1e6cd4ff396eb24385dd3974a5ba7de8f
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-07-20 19:55:59 +0000

    Update local baseURL from eu101.gitpod.io to eu102

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://samobrienol-drfspoodles-ufklcxsviem.ws-eu101.gitpod.io";
+// axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-ufklcxsviem.ws-eu102.gitpod.io";
 
-axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-ufklcxsviem.ws-eu101.gitpod.io";
-
-// axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
+axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
