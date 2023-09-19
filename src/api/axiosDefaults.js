0a38e2d44ed0f916cd0864451fcb1ac325788ed0
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-09-19 17:43:06 +0000

    Testing and updating local address re backend.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
+// axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-0h3n7m2ipr7.ws-eu104.gitpod.io/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
