84dd7e284a16ab4a103e583e7609246aa70a2f68
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-10-06 09:12:46 +0000

    Update axios.defaults.baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://spoodlespace.herokuapp.com/";
-// axios.defaults.baseURL = "https://8000-samobrienol-drfspoodles-0h3n7m2ipr7.ws-eu105.gitpod.io";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
