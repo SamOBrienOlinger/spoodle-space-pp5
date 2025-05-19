9798d4a614741afca27304201da846be968a25f4
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-05-19 10:29:19 +0000

    Update spinner to red spinning dog icon

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Spinner from "react-bootstrap/Spinner";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDog } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/Asset.module.css";
 
 const Asset = ({ spinner, src, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+      {spinner && (
+        <div className="text-center">
+          <FontAwesomeIcon icon={faDog} spin size="3x" className={styles.redIcon} />
+        </div>
+      )}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
     </div>
