4ce72948a52510b907bf09319861ca83dc8c901a
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-06-23 17:39:56 +0000

    Connected React frontend to Heroku Django backend

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
+// Set the base URL to your deployed Django backend on Heroku
+const baseURL = "https://drf-spoodle-space-38208b5fae30.herokuapp.com/";
+
+axios.defaults.baseURL = baseURL;
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+export const axiosReq = axios.create({ baseURL });
+export const axiosRes = axios.create({ baseURL });
+
+
+// import axios from "axios";
+
+// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// axios.defaults.withCredentials = true;
+
+// export const axiosReq = axios.create();
+// export const axiosRes = axios.create();
